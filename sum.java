3ede6b805fadaecc11be62bc889b899253215393
Parshva Choradia <parshchoradia0902@gmail.com>
2022-09-25 14:42:47 +0530

    Edited the sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,10 @@ public class sum{
     public static void main(String[] args) {
         int a=4;
         int b=5;
-        System.out.println(a+b);
+        System.out.println("The sum is "+a+b);
+
+        int c=10;
+        int d=11;
+        System.out.println("The Sum of 10 and 11 is "+(c+d));
     }
 }
